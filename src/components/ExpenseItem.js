206b33ee5f8acd8024189a4229e9d7f7c7a0e8ec
Jakub Nowotnik <nomeczek@gmail.com>
2023-04-15 22:27:07 +0200

    lecture 38

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,14 @@
 import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
 
-  const month = props.date.toLocaleString('pl', {month: 'long'});
-  const day = props.date.toLocaleString('pl', {day: '2-digit'});
-  const year = props.date.getFullYear();
 
   return (
     <div className='expense-item'>
-      <div>
-        <div>{day}</div>
-        <div>{month}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate
+        date={props.date}
+      />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
