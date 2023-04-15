e567888efb168d916e0ac4406e9a721236b6e3ed
Jakub Nowotnik <nomeczek@gmail.com>
2023-04-16 00:17:29 +0200

    lecture 46

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,13 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from "../UI/Card";
+import {click} from "@testing-library/user-event/dist/click";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
 
+  const clickHandler = () => {
+    console.log('Clicked!!!');
+  }
 
   return (
     <Card className='expense-item'>
@@ -14,6 +18,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>)
 }
 
