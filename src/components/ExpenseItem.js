91f9c56a1319ad29ff9c8e069c4cfac662497e36
Jakub Nowotnik <nomeczek@gmail.com>
2023-04-15 21:44:51 +0200

    lecture 35

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,17 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+
+  const expenseDate = new Date(2023, 4, 15);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 245.68;
+
   return (
     <div className='expense-item'>
-      <div>June 15th 2023</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$245.68</div>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
       </div>
     </div>)
 }
