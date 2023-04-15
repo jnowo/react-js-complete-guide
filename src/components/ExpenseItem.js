d9be93eb206364013450aef1b4817e5670b3da88
Jakub Nowotnik <nomeczek@gmail.com>
2023-04-15 21:17:39 +0200

    lecture 33

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,12 @@
 function ExpenseItem() {
-  return <h2>Expense item!</h2>
+  return (
+    <div>
+      <div>June 15 2023</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$245.68</div>
+      </div>
+    </div>)
 }
 
 export default ExpenseItem;
