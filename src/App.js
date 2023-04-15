fbeb1ba2252e34ec256d504fb32a4deeeb662231
Jakub Nowotnik <nomeczek@gmail.com>
2023-04-15 23:30:18 +0200

    task 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import ExpenseItem from './components/ExpenseItem';
 import logo from './logo.svg';
 import './App.css';
+import ExpensesList from "./components/ExpensesList";
 
 function App() {
 
@@ -28,19 +29,9 @@ function App() {
 
   return (
     <div className="App">
-      <ExpenseItem title={expenses[0].title}
-                   amount={expenses[0].amount}
-                   date={expenses[0].date}/>
-      <ExpenseItem title={expenses[1].title}
-                   amount={expenses[1].amount}
-                   date={expenses[1].date}/>
-      <ExpenseItem title={expenses[2].title}
-                   amount={expenses[2].amount}
-                   date={expenses[2].date}/>
-      <ExpenseItem title={expenses[3].title}
-                   amount={expenses[3].amount}
-                   date={expenses[3].date}/>
-
+      <ExpensesList
+        expenses={expenses}
+      />
     </div>
   );
 }
