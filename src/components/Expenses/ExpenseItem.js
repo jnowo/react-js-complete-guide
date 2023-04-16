fb938ff1c1b369c1c002eed744d083ce9b788308
Jakub Nowotnik <nomeczek@gmail.com>
2023-04-16 11:18:00 +0200

    lecture 48

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,14 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from "../UI/Card";
-import {click} from "@testing-library/user-event/dist/click";
+import {useState} from "react";
 
 const ExpenseItem = (props) => {
 
-  let title = props.title;
+  const [title, setTitle] = useState(props.title);
 
   const clickHandler = () => {
-    title = 'Updated!';
+    setTitle('Updated!');
   }
 
   return (
@@ -17,7 +17,7 @@ const ExpenseItem = (props) => {
         date={props.date}
       />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
