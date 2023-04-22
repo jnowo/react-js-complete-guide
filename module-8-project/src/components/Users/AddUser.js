206319c7066c352f03eebd2c0c3c1c3db3f02783
Jakub Nowotnik <nomeczek@gmail.com>
2023-04-22 23:42:53 +0200

    lecture 90

--- module-8-project/src/components/Users/AddUser.js
+++ module-8-project/src/components/Users/AddUser.js
@@ -1,3 +1,6 @@
+import Card from "../UI/Card";
+import styles from './AddUser.module.css';
+
 const AddUser = props => {
 
   const addUserHandler = (event) => {
@@ -5,13 +8,15 @@ const AddUser = props => {
   }
 
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor='username'>Username</label>
-      <input id='username' type='text'/>
-      <label htmlFor='age'>Age (years)</label>
-      <input id='age' type='number'/>
-      <button type='submit'>Add User</button>
-    </form>
+    <Card className={styles.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor='username'>Username</label>
+        <input id='username' type='text'/>
+        <label htmlFor='age'>Age (years)</label>
+        <input id='age' type='number'/>
+        <button type='submit'>Add User</button>
+      </form>
+    </Card>
   )
 
 }
