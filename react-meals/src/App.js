40daaeaa7f3b013dbe882f475fa1cebcbf65db62
Jakub Nowotnik <nomeczek@gmail.com>
2023-05-08 17:32:18 +0200

    lecture 134

--- react-meals/src/App.js
+++ react-meals/src/App.js
@@ -1,11 +1,15 @@
 import {Header} from "./components/Layout/Header";
+import {MealsSummary} from "./components/Meals/MealsSummary";
+import {Meals} from "./components/Meals/Meals";
 
 function App() {
   return (
     <>
       <Header/>
-    </>
-  );
+      <main>
+        <Meals/>
+      </main>
+    </>);
 }
 
 export default App;
