b510b288ac4784fd9e7b05eb0324b6e0f49bdb82
Jakub Nowotnik <nomeczek@gmail.com>
2023-05-08 17:05:48 +0200

    lecture 132

---
+++ react-meals/src/components/Layout/Header.js
@@ -0,0 +1,16 @@
+import mealsImage from '../../assets/meals.jpg';
+import styles from './Header.module.css';
+
+export const Header = (props) => {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={styles['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!'/>
+      </div>
+    </>
+  )
+}
