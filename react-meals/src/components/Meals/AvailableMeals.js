c23717ee4a389572552ca1947b3e62a2da2bd609
Jakub Nowotnik <nomeczek@gmail.com>
2023-05-11 15:57:07 +0200

    lecture 137 - fix id issue for MealItemForm.js

--- react-meals/src/components/Meals/AvailableMeals.js
+++ react-meals/src/components/Meals/AvailableMeals.js
@@ -30,7 +30,8 @@ const DUMMY_MEALS = [
 ];
 export const AvailableMeals = (params) => {
   const mealsList = DUMMY_MEALS.map(meal =>
-    <MealItem key={meal.id}
+    <MealItem id={meal.id}
+              key={meal.id}
               name={meal.name}
               description={meal.description}
               price={meal.price}/>
