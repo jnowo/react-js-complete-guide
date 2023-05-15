9d64012da270405d00759519838b19e9830f4897
Jakub Nowotnik <nomeczek@gmail.com>
2023-05-15 16:25:09 +0200

    lecture 140 - close modal button, close on backdrop click

--- react-meals/src/App.js
+++ react-meals/src/App.js
@@ -1,13 +1,25 @@
 import {Header} from "./components/Layout/Header";
 import {Meals} from "./components/Meals/Meals";
 import {Cart} from "./components/Cart/Cart";
+import {useState} from "react";
 
 function App() {
+
+  const [cartIsVisible, setCartIsVisible] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsVisible(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartIsVisible(false);
+  }
+
   return (
     <>
-      <Cart/>
-      <Header/>
-      <main>        
+      {cartIsVisible && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHandler}/>
+      <main>
         <Meals/>
       </main>
     </>);
--- react-meals/src/components/UI/Modal.js
+++ react-meals/src/components/UI/Modal.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 const Backdrop = props => {
   return (
-    <div className={styles.backdrop}/>
+    <div className={styles.backdrop} onClick={props.onClose}/>
   )
 };
 
@@ -20,7 +20,7 @@ export const Modal = props => {
 
   return (
     <>
-      {ReactDOM.createPortal(<Backdrop/>, portalElement)}
+      {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
       {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
       {}
     </>
