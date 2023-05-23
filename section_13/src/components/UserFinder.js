55bf57e8b0c7b23b260dc75c7d718d364c618d79
Jakub Nowotnik <nomeczek@gmail.com>
2023-05-23 15:24:27 +0200

    lecture 168

--- section_13/src/components/UserFinder.js
+++ section_13/src/components/UserFinder.js
@@ -1,31 +1,27 @@
-import {Fragment, useState, useEffect, Component} from 'react';
+import {Component, Fragment} from 'react';
 
 import Users from './Users';
 import classes from './UserFinder.module.css';
-
-const DUMMY_USERS = [
-  {id: 'u1', name: 'Max'},
-  {id: 'u2', name: 'Manuel'},
-  {id: 'u3', name: 'Julie'},
-];
+import UsersContext from "../store/users-context";
 
 class UserFinder extends Component {
+  static contextType = UsersContext;
   constructor() {
     super();
     this.state = {
-      filteredUsers: DUMMY_USERS,
+      filteredUsers: [],
       searchTerm: '',
     }
   }
 
   componentDidMount() {
-    this.setState({filteredUsers: DUMMY_USERS});
+    this.setState({filteredUsers: this.context.users});
   }
 
   componentDidUpdate(prevProps, prevState) {
     if (prevState.searchTerm !== this.state.searchTerm) {
       this.setState({
-        filteredUsers: DUMMY_USERS.filter((user) =>
+        filteredUsers: this.context.users.filter((user) =>
           user.name.includes(this.state.searchTerm))
       })
     }
