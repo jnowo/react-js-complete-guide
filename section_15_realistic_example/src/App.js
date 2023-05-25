1ebbc412247d4a33551002801398cbf712daf00b
Jakub Nowotnik <nomeczek@gmail.com>
2023-05-25 14:17:39 +0200

    lecture 193 - refactoring custom hook to remove external dependency

--- section_15_realistic_example/src/App.js
+++ section_15_realistic_example/src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
@@ -6,21 +6,19 @@ import {useHttp} from "./hooks/use-http";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-
-  const transformTasks = tasksObj => {
-    const loadedTasks = [];
-    for (const taskKey in tasksObj) {
-      loadedTasks.push({id: taskKey, text: tasksObj[taskKey].text});
-    }
-    setTasks(loadedTasks);
-  }
-
-  const httpData = useHttp({url: 'https://react-hooks-b996c-default-rtdb.firebaseio.com/tasks.json'}, transformTasks);
+  const httpData = useHttp();
   const {isLoading, error, sendRequest: fetchTasks} = httpData;
 
   useEffect(() => {
-    fetchTasks();
-  }, []);
+    const transformTasks = tasksObj => {
+      const loadedTasks = [];
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({id: taskKey, text: tasksObj[taskKey].text});
+      }
+      setTasks(loadedTasks);
+    };
+    fetchTasks({url: 'https://react-hooks-b996c-default-rtdb.firebaseio.com/tasks.json'}, transformTasks);
+  }, [fetchTasks]);
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
