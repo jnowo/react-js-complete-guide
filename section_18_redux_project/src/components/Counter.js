34b22b57f0931165b0b45b7ba9cf6354cedf3fb0
Jakub Nowotnik <nomeczek@gmail.com>
2023-06-01 22:28:40 +0200

    lecture 239 - added new state property (redux)

--- section_18_redux_project/src/components/Counter.js
+++ section_18_redux_project/src/components/Counter.js
@@ -5,6 +5,7 @@ import {Component} from "react";
 export const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
+  const showCounter = useSelector(state => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({type: 'increment'});
@@ -18,12 +19,13 @@ export const Counter = () => {
     dispatch({type: 'decrement'});
   }
   const toggleCounterHandler = () => {
+    dispatch({type: 'toggle'});
   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increase by 5</button>
