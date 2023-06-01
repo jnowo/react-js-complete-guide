2487023764194d5578bedbd14ed40656133ffaf2
Jakub Nowotnik <nomeczek@gmail.com>
2023-06-01 12:54:59 +0200

    lecture 222 - user friendly send order info, clear cart after sending

--- react-meals/src/components/Cart/Cart.js
+++ react-meals/src/components/Cart/Cart.js
@@ -7,6 +7,8 @@ import {Checkout} from "./Checkout";
 
 export const Cart = props => {
   const [isCheckout, setIsCheckout] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [didSubmit, setDidSubmit] = useState(false);
   const cartCtx = useContext(CartContext);
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
@@ -24,35 +26,38 @@ export const Cart = props => {
     setIsCheckout(true);
   }
 
-  const submitOrderHandler = (userData) => {
-    fetch(`${process.env.REACT_APP_FIREBASE_URL}orders.json`, {
+  const submitOrderHandler = async (userData) => {
+    setIsSubmitting(true);
+    await fetch(`${process.env.REACT_APP_FIREBASE_URL}orders.json`, {
       method: 'POST',
       body: JSON.stringify({
         user: userData,
         orderedItems: cartCtx.items
       })
-    })
+    });
+    setIsSubmitting(false);
+    setDidSubmit(true);
+    cartCtx.clearCart();
   }
 
-  const cartItems = <ul className={styles['cart-items']}>{cartCtx.items
-    .map((item) => (<CartItem
-      key={item.id}
-      name={item.name}
-      amount={item.amount}
-      price={item.price}
-      onRemove={cartItemRemoveHandler.bind(null, item.id)}
-      //bind ensures that id of removed item is passed to remove handler.
-      onAdd={cartItemAddHandler.bind(null, item)}/>)
-    )}</ul>
-  ;
+  const cartItems = (<ul className={styles['cart-items']}>{cartCtx.items
+      .map((item) => (<CartItem
+        key={item.id}
+        name={item.name}
+        amount={item.amount}
+        price={item.price}
+        onRemove={cartItemRemoveHandler.bind(null, item.id)}
+        //bind ensures that id of removed item is passed to remove handler.
+        onAdd={cartItemAddHandler.bind(null, item)}/>)
+      )}</ul>
+  );
 
   const modalActions = <div className={styles.actions}>
     <button className={styles['button--alt']} onClick={props.onClose}>Close</button>
     {hasItems && <button className={styles.button} onClick={orderHandler}>Order</button>}
   </div>;
 
-  return (
-    <Modal onClose={props.onClose}>
+  const cartModalContetnt = (<>
       {cartItems}
       <div className={styles.total}>
         <span>Total Amount</span>
@@ -60,6 +65,21 @@ export const Cart = props => {
       </div>
       {isCheckout && <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose}/>}
       {!isCheckout && modalActions}
+    </>
+  );
+
+  const isSubmittingContent = <p>Sending order data...</p>;
+  const didSubmitModalContent = <><p>Successfully sent the order!</p>
+    <div className={styles.actions}>
+      <button className={styles.button} onClick={props.onClose}>Close</button>
+    </div>
+  </>;
+
+  return (
+    <Modal onClose={props.onClose}>
+      {!isSubmitting && !didSubmit && cartModalContetnt}
+      {isSubmitting && isSubmittingContent}
+      {!isSubmitting && didSubmit && didSubmitModalContent}
     </Modal>
   );
 }
