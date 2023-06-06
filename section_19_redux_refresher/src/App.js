80e716b029fb111c2dbbc774a48d5955cb97e31c
Jakub Nowotnik <nomeczek@gmail.com>
2023-06-06 15:03:31 +0200

    lecture 257 - send cart data to firebase

--- section_19_redux_refresher/src/App.js
+++ section_19_redux_refresher/src/App.js
@@ -2,10 +2,20 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import {useSelector} from "react-redux";
+import {useEffect} from "react";
 
 function App() {
 
   const cartIsVisible = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_FIREBASE_URL}cart.json`, {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    })
+  }, [cart]);
+
 
   return (
     <Layout>
