8d65bb1a3fba2f12c9f7e2ff09d047a93ecc8613
Jakub Nowotnik <nomeczek@gmail.com>
2023-06-06 23:57:02 +0200

    lecture 270 - simple routes

---
+++ section_20_react_router/src/pages/Home.js
@@ -0,0 +1,5 @@
+export const HomePage = () => {
+  return (
+    <h1>The Home Page</h1>
+  );
+}
---
+++ section_20_react_router/src/pages/Products.js
@@ -0,0 +1,5 @@
+export const ProductsPage = () => {
+  return (
+    <h1>The Products Page</h1>
+  );
+}
