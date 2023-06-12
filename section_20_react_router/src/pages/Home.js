868b6279aa3d874d725339d5b877a3b00658413f
Jakub Nowotnik <nomeczek@gmail.com>
2023-06-12 12:36:51 +0200

    lecture 276 - navigate programmatically

--- section_20_react_router/src/pages/Home.js
+++ section_20_react_router/src/pages/Home.js
@@ -1,11 +1,20 @@
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 export const HomePage = () => {
+  const navigate = useNavigate();
+
+  const navigateHandler = () => {
+    navigate('/products');
+  }
+
   return (
     <>
       <h1>The Home Page</h1>
       <p>
         Go to <Link to='/products'>the list of products</Link>.
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Go to products</button>
       </p>
     </>
   );
