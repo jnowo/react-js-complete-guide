2784ca5304d477cb65044a90662a1f5f0c1254c6
Jakub Nowotnik <nomeczek@gmail.com>
2023-06-12 15:25:18 +0200

    lecture 279 - difference between relative and absolute path

--- section_20_react_router/src/pages/Home.js
+++ section_20_react_router/src/pages/Home.js
@@ -4,14 +4,14 @@ export const HomePage = () => {
   const navigate = useNavigate();
 
   const navigateHandler = () => {
-    navigate('/products');
+    navigate('products');
   }
 
   return (
     <>
       <h1>The Home Page</h1>
       <p>
-        Go to <Link to='/products'>the list of products</Link>.
+        Go to <Link to='products'>the list of products</Link>.
       </p>
       <p>
         <button onClick={navigateHandler}>Go to products</button>
