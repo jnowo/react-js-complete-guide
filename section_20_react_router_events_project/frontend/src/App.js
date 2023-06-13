8817648e9715761904258e3a7a2a45473f5d3aaa
Jakub Nowotnik <nomeczek@gmail.com>
2023-06-13 18:50:06 +0200

    lecture 319 - move loader function to EventsPage

--- section_20_react_router_events_project/frontend/src/App.js
+++ section_20_react_router_events_project/frontend/src/App.js
@@ -23,7 +23,7 @@
 
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import {HomePage} from "./pages/HomePage";
-import {EventsPage} from "./pages/EventsPage";
+import {EventsPage, loader as eventsLoader} from "./pages/EventsPage";
 import {EventDetailPage} from "./pages/EventDetailPage";
 import {NewEventPage} from "./pages/NewEventPage";
 import {EditEventPage} from "./pages/EditEventPage";
@@ -39,15 +39,7 @@ const router = createBrowserRouter([
       {
         path: 'events', element: <EventsRoot/>, children: [
           {
-            index: true, element: <EventsPage/>, loader: async () => {
-              const response = await fetch('http://localhost:8080/events');
-
-              if (!response.ok) {
-              } else {
-                const resData = await response.json();
-                return resData.events;
-              }
-            }
+            index: true, element: <EventsPage/>, loader: eventsLoader
           },
           {path: ':id', element: <EventDetailPage/>},
           {path: ':id/edit', element: <EditEventPage/>},
