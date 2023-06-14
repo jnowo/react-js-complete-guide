a988bdf1ea4d521bac23bff7fb3e72de3484ad0b
Jakub Nowotnik <nomeczek@gmail.com>
2023-06-14 12:29:21 +0200

    lecture 324 - handle error with redux loader

---
+++ section_20_react_router_events_project/frontend/src/pages/Error.js
@@ -0,0 +1,7 @@
+export const ErrorPage = () => {
+return (
+    <section>
+      <h1>Error Page</h1>
+    </section>
+  );
+}
--- section_20_react_router_events_project/frontend/src/pages/EventsPage.js
+++ section_20_react_router_events_project/frontend/src/pages/EventsPage.js
@@ -3,6 +3,10 @@ import {useLoaderData} from "react-router-dom";
 
 export const EventsPage = () => {
   const data = useLoaderData();
+
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
   const events = data.events;
   return (
     <>
@@ -15,6 +19,8 @@ export const loader = async () => {
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
+    // return {isError: true, message: 'Something went wrong!'};
+    throw new Error('Something went wrong!'); //react-router-dom will catch this error and display the closest errorElment (will bubble up)
   } else {
     return response;
   }
