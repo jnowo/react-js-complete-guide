2cbae98325ff37eebbbf1f3c3ede05d448331240
Jakub Nowotnik <nomeczek@gmail.com>
2023-06-14 13:17:03 +0200

    lecture 326 - use json() instead of new Response on error

--- section_20_react_router_events_project/frontend/src/pages/EventsPage.js
+++ section_20_react_router_events_project/frontend/src/pages/EventsPage.js
@@ -1,5 +1,5 @@
 import EventsList from '../components/EventsList';
-import {useLoaderData} from "react-router-dom";
+import {json, useLoaderData} from "react-router-dom";
 
 export const EventsPage = () => {
   const data = useLoaderData();
@@ -21,7 +21,8 @@ export const loader = async () => {
   if (!response.ok) {
     // return {isError: true, message: 'Something went wrong!'};
     // throw new Error('Something went wrong!'); //react-router-dom will catch this error and display the closest errorElment (will bubble up)
-    throw new Response(JSON.stringify({message: 'Could not fetch events!'}), {status: 500});
+    // throw new Response(JSON.stringify({message: 'Could not fetch events!'}), {status: 500});
+    return json({message: 'Could not fetch events!'}, {status: 500});
   } else {
     return response;
   }
