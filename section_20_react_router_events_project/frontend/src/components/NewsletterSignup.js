b3cf0c0c79b0ec1bad83d84be23303d09a7c4780
Jakub Nowotnik <nomeczek@gmail.com>
2023-06-25 01:06:47 +0200

    lecture 337 - useFetcher - run action without route transition

--- section_20_react_router_events_project/frontend/src/components/NewsletterSignup.js
+++ section_20_react_router_events_project/frontend/src/components/NewsletterSignup.js
@@ -1,15 +1,31 @@
 import classes from './NewsletterSignup.module.css';
+import {useFetcher} from "react-router-dom";
+import {useEffect} from "react";
 
 function NewsletterSignup() {
+
+  const fetcher = useFetcher();
+  // fetcher.Form still trigger action, but it will not initialize route transition
+  // fetcher should be used whenever you want to trigger an action but not initialize route transition
+  // use when you want to trigger action or loader without loading the page (ROUTE TRANSITION) to which this action/loader belongs
+  const {data, state} = fetcher;
+
+  useEffect(() => {
+      if (state === 'idle' && data && data.message) {
+        window.alert(data.message);
+      }
+    }, [data, state]
+  )
+  ;
   return (
-    <form method="post" className={classes.newsletter}>
+    <fetcher.Form method="post" action='/newsletter' className={classes.newsletter}>
       <input
         type="email"
         placeholder="Sign up for newsletter..."
         aria-label="Sign up for newsletter"
       />
       <button>Sign up</button>
-    </form>
+    </fetcher.Form>
   );
 }
 
