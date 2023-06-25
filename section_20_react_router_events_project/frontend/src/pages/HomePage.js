a45709fc55d4e2ffc6cae8979f53ca44b412016e
Jakub Nowotnik <nomeczek@gmail.com>
2023-06-25 17:07:31 +0200

    lecture 339 - controlling which data should be deferred

--- section_20_react_router_events_project/frontend/src/pages/HomePage.js
+++ section_20_react_router_events_project/frontend/src/pages/HomePage.js
@@ -1,7 +1,9 @@
+import PageContent from "../components/PageContent";
+
 export const HomePage = () => {
   return (
-    <section>
-      <h1>Home Page</h1>
-    </section>
+    <PageContent title='Welcome'>
+      <p>Browse all our amazing events!</p>
+    </PageContent>
   );
 }
