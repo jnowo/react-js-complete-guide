9bdee94b0a140cfd1784fe0a81fab88088f63733
Jakub Nowotnik <nomeczek@gmail.com>
2023-06-27 11:52:31 +0200

    lecture 348 - add auth action for authForm

--- section_22_authentication/frontend/src/pages/Authentication.js
+++ section_22_authentication/frontend/src/pages/Authentication.js
@@ -1,7 +1,48 @@
 import AuthForm from '../components/AuthForm';
+import {json, redirect} from "react-router-dom";
 
 function AuthenticationPage() {
-  return <AuthForm />;
+  return <AuthForm/>;
 }
 
 export default AuthenticationPage;
+
+//this action will always be triggered when authForm is submitted
+export const action = async ({request}) => {
+  const searchParams = new URL(request.url).searchParams;
+  const mode = searchParams.get('mode') || 'login';
+
+  if (mode !== 'login' && mode !== 'signup') {
+    throw json({message: 'Invalid mode'}, {status: 422});
+  }
+
+  const data = await request.formData();
+
+  const authData = {
+    email: data.get('email'),
+    password: data.get('password')
+  }
+
+  const response = await fetch('http://localhost:8080/' + mode,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(authData)
+    });
+
+
+  if (response.status === 422 || response.status === 401) {
+    //we can return response directly, react router will extract data for us
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({message: 'Could not authenticate you!'}, {status: 500});
+  }
+
+  //soon manage token
+
+  return redirect('/');
+}
