254142eb17b08fce19e878066deffbe8328d5f56
Jakub Nowotnik <nomeczek@gmail.com>
2023-06-29 14:17:40 +0200

    lecture 362 - use Suspense component for lazy lading components, use meta object returned by router and pass it to loader

--- section_23_project/src/App.js
+++ section_23_project/src/App.js
@@ -1,24 +1,37 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 
-import BlogPage, { loader as postsLoader } from './pages/Blog';
+// import BlogPage, {loader as postsLoader} from './pages/Blog';
 import HomePage from './pages/Home';
-import PostPage, { loader as postLoader } from './pages/Post';
+// import PostPage, {loader as postLoader} from './pages/Post';
 import RootLayout from './pages/Root';
+import {lazy, Suspense} from "react";
+
+const BlogPage = lazy(() => import('./pages/Blog'));
+const PostPage = lazy(() => import('./pages/Post'));
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <RootLayout />,
+    element: <RootLayout/>,
     children: [
       {
         index: true,
-        element: <HomePage />,
+        element: <HomePage/>,
       },
       {
         path: 'posts',
         children: [
-          { index: true, element: <BlogPage />, loader: postsLoader },
-          { path: ':id', element: <PostPage />, loader: postLoader },
+          //we can use import() to load the component dynamically, only when it is needed (when the user navigates to the page)
+          {
+            index: true,
+            element: <Suspense fallback={<p>Loading...</p>}><BlogPage/></Suspense>,
+            loader: () => import('./pages/Blog').then(module => module.loader())
+          },
+          {
+            path: ':id',
+            element: <Suspense fallback={<p>Loading...</p>}><PostPage/></Suspense>,
+            loader: (meta) => import('./pages/Post').then(module => module.loader(meta)),
+          },
         ],
       },
     ],
@@ -26,7 +39,7 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
