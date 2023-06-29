248e5017ec546237ee65ef5633fb8c120b3837b9
Jakub Nowotnik <nomeczek@gmail.com>
2023-06-30 00:26:22 +0200

    lecture 378 - using Link trag from next.js (no page reload) similar ro React Router Link

--- section_24_nextjs/pages/news/index.js
+++ section_24_nextjs/pages/news/index.js
@@ -1,7 +1,17 @@
 //our-domain.com/news
 
+import Link from "next/link";
+
 const NewsPage = () => {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li><Link href='/news/1'>Next.js is a great framework</Link></li>
+        <li>Something else</li>
+      </ul>
+    </>
+  );
 }
 
 export default NewsPage;
