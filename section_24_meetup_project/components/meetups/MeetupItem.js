8fa19f7ed6053c22f62a76dbd9f15e742ae82170
Jakub Nowotnik <nomeczek@gmail.com>
2023-08-05 14:44:39 +0200

    lecture 385 - meetup detail page + css

--- section_24_meetup_project/components/meetups/MeetupItem.js
+++ section_24_meetup_project/components/meetups/MeetupItem.js
@@ -1,13 +1,13 @@
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
-import {useRouter} from "next/router";
+import {useRouter} from "next/navigation";
 
 function MeetupItem(props) {
 
   const router = useRouter();
 
   function showDetailsHandler() {
-    router.push('/' + props.id);
+    router.push(`/${props.id}`);
   }
 
   return (
--- section_24_meetup_project/pages/[meetupId]/index.js
+++ section_24_meetup_project/pages/[meetupId]/index.js
@@ -1,8 +1,13 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
 function MeetupDetails() {
   return (
-    <>
-      <img src={} alt={}/>
-    </>
+    <MeetupDetail
+      img='https://warsawtour.pl/wp-content/uploads/2018/07/Centrum-fot.-Zbigniew-Pan%C3%B3w-pzstudio.pl_.jpg'
+      title="The First Meetup"
+      address="Some Street 5, Some City"
+      description="This is a firsy meetup"
+    />
   );
 }
 
