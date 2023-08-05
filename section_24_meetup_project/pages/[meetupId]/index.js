6c86ae78b13618dc0baf6f8bd0bbe85580e091d9
Jakub Nowotnik <nomeczek@gmail.com>
2023-08-05 19:05:14 +0200

    lecture 390 - params for SSG

--- section_24_meetup_project/pages/[meetupId]/index.js
+++ section_24_meetup_project/pages/[meetupId]/index.js
@@ -11,4 +11,22 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticProps(context) {
+  //fetch data for single meetup
+
+  const meetupId = context.params.meetupId;
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        img: 'https://warsawtour.pl/wp-content/uploads/2018/07/Centrum-fot.-Zbigniew-Pan%C3%B3w-pzstudio.pl_.jpg',
+        title: 'The First Meetup',
+        address: 'Some Street 5, Some City',
+        description: 'This is a first meetup',
+      }
+    }
+  }
+}
+
 export default MeetupDetails;
