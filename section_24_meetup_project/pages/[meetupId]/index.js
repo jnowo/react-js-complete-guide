564f536d4b1d9c58571369663053acc31f72c9b7
Jakub Nowotnik <nomeczek@gmail.com>
2023-08-06 01:13:18 +0200

    lecture 396 - getting meetup detail data from database

--- section_24_meetup_project/pages/[meetupId]/index.js
+++ section_24_meetup_project/pages/[meetupId]/index.js
@@ -1,47 +1,52 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
+import {MongoClient, ObjectId} from "mongodb";
+import {mongoCredentials} from "../../data/mongo-utils";
 
-function MeetupDetails() {
+function MeetupDetails(props) {
   return (
     <MeetupDetail
-      img='https://warsawtour.pl/wp-content/uploads/2018/07/Centrum-fot.-Zbigniew-Pan%C3%B3w-pzstudio.pl_.jpg'
-      title="The First Meetup"
-      address="Some Street 5, Some City"
-      description="This is a firsy meetup"
+      img={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 }
 
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(mongoCredentials);
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find({}, {_id: 1}).toArray();
+
+  await client.close();
+
   return {
-    fallback: true, //tells next.js weather paths contains all supported values or just some of theme
-    paths: [
-      {
-        params: {
-          meetupId: 'm1'
-        }
-      },
-      {
-        params: {
-          meetupId: 'm2'
-        }
-      },
-    ]
+    fallback: false, //tells next.js weather paths contains all supported values or just some of theme
+    paths: meetups.map(meetup => ({
+      params: {meetupId: meetup._id.toString()}
+    })),
   }
 }
 
 export async function getStaticProps(context) {
-  //fetch data for single meetup
+  const meetupId = new ObjectId(context.params.meetupId);
+  const client = await MongoClient.connect(mongoCredentials);
+  const db = client.db();
 
-  const meetupId = context.params.meetupId;
-  console.log(meetupId);
+  const meetupsCollection = db.collection('meetups');
+
+  const selectedMeetup = await meetupsCollection.findOne({_id: meetupId});
+
+  await client.close();
 
   return {
     props: {
       meetupData: {
-        img: 'https://warsawtour.pl/wp-content/uploads/2018/07/Centrum-fot.-Zbigniew-Pan%C3%B3w-pzstudio.pl_.jpg',
-        title: 'The First Meetup',
-        address: 'Some Street 5, Some City',
-        description: 'This is a first meetup',
+        ...selectedMeetup.data,
+        id: selectedMeetup._id.toString(),
       }
     }
   }
