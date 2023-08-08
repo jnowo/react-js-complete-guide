3376dd1859f11fb52b503fbd47d2462c66993cce
Jakub Nowotnik <nomeczek@gmail.com>
2023-08-08 14:40:48 +0200

    lecture 437 - testing, init

---
+++ section_24_meetup_project/data/mongo-utils.js
@@ -0,0 +1,2 @@
+// export const mongoCredentials = 'mongodb://udemy-react:Udemy123!@192.168.88.240:27017/udemy-react';
+export const mongoCredentials = 'mongodb://root:example@192.168.88.240:27017/meetups';
