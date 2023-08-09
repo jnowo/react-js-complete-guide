a43819406fc59847d79c2ed0a05fa80dad29bdff
Jakub Nowotnik <nomeczek@gmail.com>
2023-08-09 23:52:14 +0200

    lecture 441 - testing Greeting.js with userEvent

--- section_27_testing_react_app/src/component/Greeting.js
+++ section_27_testing_react_app/src/component/Greeting.js
@@ -1,9 +1,19 @@
+import {useState} from "react";
+
 export const Greeting = () => {
+
+  const [changedText, setChangedText] = useState(false);
+
+  function changeTextHandler() {
+    setChangedText(true);
+  }
 
   return (
     <div>
       <h1>Hello world!</h1>
-      <p1>It's good to see you!</p1>
+      {!changedText && <p1>It's good to see you!</p1>}
+      {changedText && <p1>Changed!</p1>}
+      <button onClick={changeTextHandler}>Change text</button>
     </div>
   )
 
--- section_27_testing_react_app/src/component/Greeting.test.js
+++ section_27_testing_react_app/src/component/Greeting.test.js
@@ -1,5 +1,7 @@
 import {render, screen} from "@testing-library/react";
 import {Greeting} from "./Greeting";
+import userEvent from "@testing-library/user-event";
+import {act} from "react-dom/test-utils";
 
 describe('Greeting component tests', () => {
   test('renders Hello World as a text', () => {
@@ -13,6 +15,41 @@ describe('Greeting component tests', () => {
     //Access to virtual DOM
     const helloWorldElement = screen.getByText('Hello world!', {exact: true});
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test('renders good to see you if the button was NOT clicked', () => {
+    render(<Greeting/>);
+
+    const outputElement = screen.getByText('good to see you', {exact: false});
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('render "Changed!" if the button was clicked', () => {
+    render(<Greeting/>);
+
+    //Act
+    const buttonElement = screen.getByRole('button');
+    act(() => {
+      userEvent.click(buttonElement)
+    });
+
+    //Assert
+    const outputElement = screen.getByText('Changed!', {exact: false});
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('greeting component does not render "Good to see you!" when button was clicked', () => {
+    render(<Greeting/>);
+
+    //Act
+    const buttonElement = screen.getByRole('button');
+    act(() => {
+      userEvent.click(buttonElement)
+    });
+
+    //Assert
+    const outputElement = screen.queryByText('good to see you!', {exact: false});
+    expect(outputElement).toBeNull();
   });
 });
 
