6bbd09c7a9d1bc45e6ba28717ad64d634f531b06
Jakub Nowotnik <nomeczek@gmail.com>
2023-08-10 00:19:44 +0200

    lecture 443 - working with mocks

--- section_27_testing_react_app/src/component/Async.test.js
+++ section_27_testing_react_app/src/component/Async.test.js
@@ -4,6 +4,10 @@ import {render, screen} from "@testing-library/react";
 describe('Async component', () => {
 
   test('renders posts if request succeeds', async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{id: 'p1', title: 'First post'}]
+    });
     render(<Async/>);
 
     // const listItemElements = screen.getByRole('listitem');
