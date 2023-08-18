535df71650983d5f624722395ad883d105af762a
Jakub Nowotnik <nomeczek@gmail.com>
2023-08-18 22:46:24 +0200

    lecture 468 - remove todo

--- section_28_react_and_typescript/src/components/Todos.tsx
+++ section_28_react_and_typescript/src/components/Todos.tsx
@@ -4,11 +4,13 @@ import {TodoItem} from "./TodoItem";
 import classes from "./Todos.module.css";
 
 //FC - function that act as a functional component
-export const Todos: FC<{ items: Todo[] }> = (props) => {
+export const Todos: FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (props) => {
     return (
         <ul className={classes.todos}>
             {props.items.map((todo) => (
-                <TodoItem key={todo.id} text={todo.text}/>
+                <TodoItem key={todo.id}
+                          text={todo.text}
+                          onRemoveTodo={props.onRemoveTodo.bind(null, todo.id)}/>
             ))}
         </ul>
     )
